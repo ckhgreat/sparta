a5fc99ec5611f5588e8171e22507ac1830a95f0b
KyuHyun Chung <ckhgreat@naver.com>
2020-03-29 18:20:56 +0900

    ..

--- genie.py
+++ genie.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',headers=headers)
 
@@ -10,7 +9,6 @@ songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr'
 
 num = 1
 for song in songs:
-    # movie 안에 a 가 있으면,
     a_tag = song.select_one('td.info > a')
     if a_tag is not None:
         print(num, a_tag.text.strip())
